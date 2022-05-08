e898f2fbf47b953db9b384695bec9748a36322bc
AndriusMatulionis <89296933+AndriusMatulionis@users.noreply.github.com>
2022-05-08 09:43:09 +0300

    Atvaizduoja spanus

--- scrape1.py
+++ scrape1.py
@@ -4,22 +4,22 @@ import pandas as pd
 
 
 count = 1
-product_type = 'dji+mavic+air' #change product type here
 df = pd.DataFrame(columns=['Nr', 'Product', 'Price', 'Location'])
 
 
-for i in range(1, 3): #number of pages
-    url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw='+product_type+'&_ipg=240&_pgn='+str(i)+''
+for i in range(1, 4): #number of pages
+    url = f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=drones&_sacat={0}'
     data = requests.get(url).text
     soup = BeautifulSoup(data, 'html.parser')
     products = soup.find_all('div', class_='s-item__info clearfix')
-    #location = soup.find_all('div', class_='s-item__info clearfix')
+    
 
     for product in products:
         Nr = count
         Product = product.h3.text
         Price = product.find('span', class_='s-item__price').text
         Location = product.find('span', class_='s-item__location s-item__itemLocation')
+           
         df = df.append(
             {'Nr': count, 
              'Product': Product, 
@@ -27,6 +27,11 @@ for i in range(1, 3): #number of pages
              'Location': Location},
             ignore_index=True)
         count += 1
+        
+
 columns = ['Nr', 'Product', 'Price', 'Location']
-df.to_csv('out.csv', encoding='utf-8', index=False, sep=';', columns=columns)
+
+
+#df.to_csv('out.csv', encoding='utf-8', index=False, columns=columns)
 print(df)
+
