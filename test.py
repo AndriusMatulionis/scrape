a3ae52589aeae0e1ad999b28dee8811a1e8c5202
AndriusMatulionis <89296933+AndriusMatulionis@users.noreply.github.com>
2022-05-08 16:20:34 +0300

    2022.05.08

--- test.py
+++
@@ -1,7 +0,0 @@
-my_string="Delft, Stack, Netherlands"
-print("Original String is:")
-print(my_string)
-
-transformed_string=my_string.replace(",","")
-print("Transformed String is:")
-print(transformed_string)
