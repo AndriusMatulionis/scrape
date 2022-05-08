26c02c5bf5c7d07fba21babb12bb97dd03f4c50b
AndriusMatulionis <89296933+AndriusMatulionis@users.noreply.github.com>
2022-05-08 15:15:38 +0300

    su  <span class=............ issaugo i csv, reikia taisyt

--- scrape2.py
+++ scrape2.py
@@ -1,45 +1,29 @@
-import bs4
-import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import requests
+from csv import writer
 
-count = 1
-product_type = 'dji+mavic+air'
+with open('scraped.csv', 'w', encoding='utf-8', newline='') as f:
+    thewriter = writer(f)
+    header = ['Product', 'Price', 'Location']
+    thewriter.writerow(header)  
+
+    for i in range(1, 2):
+        url = f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=drones&_sacat={0}'
+        page = requests.get(url).text.strip()
+        soup = BeautifulSoup(page, 'html.parser')
+        products = soup.find_all('div', class_='s-item__info clearfix')
 
 
-url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=dji+mavic+air2s&_sacat=0&LH_TitleDesc=0&_odkw=drones&_osacat=0'
-
-def get_data(url):
-    r = requests.get(url)
-    soup = bs(r.text, 'html.parser')
-    return soup 
-
-def parse(soup):
-    productslist = []
-    results = soup.find_all('div', {'class': 's-item__info clearfix'})
-    for item in results:
-        NR = count
-        product = {
-            'Nr': count,
-            'Product': item.find('h3', class_='s-item__title').text,
-            'Price': item.find('span', class_='s-item__price').text,
-            'Location': item.find('span', class_='s-item__location s-item__itemLocation').text,
-            'Link': item.find('a', class_='s-item__link')['href'], 
-        }           
-        productslist.append(product)
-    return productslist   
-
-def output(productslist):
-    productsdf = pd.DataFrame(productslist)
-    productsdf.to_csv('out1.csv', encoding='utf-8', index=False, sep=';', columns=columns)
-    print('Saved to CSV')
-    return
-
-soup = get_data(url)
-productslist = parse(soup)
-output(productslist)
-print(parse(soup))
-
-    
-      
-    
+        
+        for product in products:
+            Product = product.h3.text
+            Price = product.find('span', class_='s-item__price').text
+            Location = product.find('span', class_='s-item__location s-item__itemLocation')
+            if Location is None:
+                print('element not found')
+                thewriter.writerow('element not found')
+            else:
+                thewriter.writerow(Location.text)    
+            info = [Product, Price, Location]
+            print(info)
+            
--- test.py
+++ test.py
@@ -1,44 +1,7 @@
-import bs4 as bs
-import requests
-import pandas as pd
+my_string="Delft, Stack, Netherlands"
+print("Original String is:")
+print(my_string)
 
-url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=2017+patrick+mahomes+psa+10+auto&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1&_ipg=100'
-
-def get_data(url):
-    r = requests.get(url)
-    soup = bs(r.text, 'html.parser')
-    return soup
-
-def parse(soup):
-    productslist = []
-    results = soup.find_all('div', {'class': 's-item__info clearfix'})
-    for item in results:
-        
-        try:
-            bids = item.find('span', class_='s-item__bids s-item__bidCount').text
-        except:
-            bids = ''
-
-        product = {
-            'title': item.find('h3', class_='s-item__title s-item__title--has-tags').text,
-            'soldprice': float(item.find('span', class_='s-item__price').text.replace('$', '').replace(',','').strip()),
-            'solddate': item.find('span', class_='s-item__title--tagblock__COMPLETED').find('span', class_='POSITIVE').text,
-            'bids': bids,
-            'link': item.find('a', class_='s-item__link')['href'],
-            }
-        productslist.append(product)
-    return productslist
-
-def output(productslist):
-    productsdf = pd.DataFrame(productslist)
-    productsdf.to_csv('2017_Patrick_Mahomes_Rookies.csv', index=False)
-    return productslist
-    print(productsdf ('Saved to CSV'))
-    
-
-
-
-#soup = get_data(url)
-#productslist = parse(soup)
-#output(productslist)
-#print(parse(soup))
+transformed_string=my_string.replace(",","")
+print("Transformed String is:")
+print(transformed_string)
