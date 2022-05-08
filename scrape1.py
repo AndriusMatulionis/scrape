48971fa41882f87d122bc379c597ce957f10ec52
AndriusMatulionis <89296933+AndriusMatulionis@users.noreply.github.com>
2022-05-08 09:48:31 +0300

    Su komentarais

--- scrape1.py
+++ scrape1.py
@@ -8,7 +8,7 @@ df = pd.DataFrame(columns=['Nr', 'Product', 'Price', 'Location'])
 
 
 for i in range(1, 4): #number of pages
-    url = f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=drones&_sacat={0}'
+    url = f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=drones&_sacat={0}'    # f'{0}  formatuoja adreso paskutini puslapio skaiciu, kuris per cikla gali keistis. 
     data = requests.get(url).text
     soup = BeautifulSoup(data, 'html.parser')
     products = soup.find_all('div', class_='s-item__info clearfix')
@@ -18,7 +18,7 @@ for i in range(1, 4): #number of pages
         Nr = count
         Product = product.h3.text
         Price = product.find('span', class_='s-item__price').text
-        Location = product.find('span', class_='s-item__location s-item__itemLocation')
+        Location = product.find('span', class_='s-item__location s-item__itemLocation').text    # Terminale laustiniuose skliaustuose grazina location, csv exportina su spanais. Nepriima atributo .text
            
         df = df.append(
             {'Nr': count, 
@@ -29,9 +29,9 @@ for i in range(1, 4): #number of pages
         count += 1
         
 
-columns = ['Nr', 'Product', 'Price', 'Location']
+        columns = ['Nr', 'Product', 'Price', 'Location']
 
 
-#df.to_csv('out.csv', encoding='utf-8', index=False, columns=columns)
+df.to_csv('out.csv', encoding='utf-8', index=False, columns=columns)
 print(df)
 
